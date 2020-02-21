48a7fa2a2aa212e03ad63a829bb76bf0fd5ccc7c
Jesse Jackson <jsejcksn@users.noreply.github.com>
2020-02-21 11:37:16 -0600

    Explicity handle lack of location data

--- api/weather.ts
+++ api/weather.ts
@@ -35,24 +35,36 @@ type ApiResponse = {
 
 export default async (req: NowRequest, res: NowResponse): Promise<void> => {
   try {
-    let units = Units.Metric;
     let unitsInput: string;
     if (Array.isArray(req.query.units)) unitsInput = req.query.units[0];
     else unitsInput = req.query.units;
+
+    let units = Units.Metric;
     const unitsList: string[] = [...Object.values(Units)];
     if (unitsList.includes(unitsInput)) units = unitsInput as Units;
+
     const params: {[key: string]: string} = {...req.query, units};
     if (!params.zip && !params.q && !params.lat && !params.lon) {
-      const ip: string = req.headers['x-forwarded-for'];
+      const xForwardedFor = req.headers['x-forwarded-for'];
+      if (typeof xForwardedFor !== 'string') {
+        throw new Error('No location data available');
+      }
+      const ip = xForwardedFor as string;
       const [lat, lon] = await fetchCoordinates(ip);
       [params.lat, params.lon] = [String(lat), String(lon)];
     }
+
     const responseData = await fetchCurrentWeather(params);
     const {
-      coord: {lat, lon}, dt,
-      main: {feels_like, humidity, temp, temp_max, temp_min}, name,
-      sys: {country, sunrise, sunset}, timezone, weather,
+      coord: {lat, lon},
+      dt,
+      main: {feels_like, humidity, temp, temp_max, temp_min},
+      name,
+      sys: {country, sunrise, sunset},
+      timezone,
+      weather,
     } = responseData;
+
     const data: ApiResponse = {
       country,
       dt,
