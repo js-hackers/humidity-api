4150629ab9119c8b712763656f94f89dd42388bf
Jesse Jackson <jsejcksn@users.noreply.github.com>
2020-02-23 13:17:54 -0600

    Return data for clouds, pressure, rain, snow, wind

--- api/current.ts
+++ api/current.ts
@@ -11,21 +11,29 @@ export enum Units {
 }
 
 export type CurrentWeatherData = {
+  clouds_all: number;
   country: string;
   dt: number;
-  feels_like: number;
   humidity: number;
   lat: number;
   lon: number;
   name: string;
+  pressure: number;
+  rain_1h: number;
+  rain_3h: number;
+  snow_1h: number;
+  snow_3h: number;
   sunrise: number;
   sunset: number;
+  temp: number;
+  temp_feels_like: number;
   temp_max: number;
   temp_min: number;
-  temp: number;
   timezone: number;
   units: Units;
   weather: WeatherCondition[];
+  wind_deg: number;
+  wind_speed: number;
 };
 
 export default async (req: NowRequest, res: NowResponse): Promise<void> => {
@@ -51,32 +59,32 @@ export default async (req: NowRequest, res: NowResponse): Promise<void> => {
       [params.lat, params.lon] = [String(lat), String(lon)];
     }
 
-    const {
-      coord: {lat, lon},
-      dt,
-      main: {feels_like, humidity, temp, temp_max, temp_min},
-      name,
-      sys: {country, sunrise, sunset},
-      timezone,
-      weather,
-    } = await fetchCurrentWeather(params);
+    const responseData = await fetchCurrentWeather(params);
 
-    const data = {
-      country,
-      dt,
-      feels_like,
-      humidity,
-      lat,
-      lon,
-      name,
-      sunrise,
-      sunset,
-      temp,
-      temp_max,
-      temp_min,
-      timezone,
+    const data: CurrentWeatherData = {
+      clouds_all: responseData.clouds.all,
+      country: responseData.sys.country,
+      dt: responseData.dt,
+      humidity: responseData.main.humidity,
+      lat: responseData.coord.lat,
+      lon: responseData.coord.lon,
+      name: responseData.name,
+      pressure: responseData.main.pressure,
+      rain_1h: responseData.rain?.['1h'] || 0,
+      rain_3h: responseData.rain?.['3h'] || 0,
+      snow_1h: responseData.snow?.['1h'] || 0,
+      snow_3h: responseData.snow?.['3h'] || 0,
+      sunrise: responseData.sys.sunrise,
+      sunset: responseData.sys.sunset,
+      temp: responseData.main.temp,
+      temp_feels_like: responseData.main.feels_like,
+      temp_max: responseData.main.temp_max,
+      temp_min: responseData.main.temp_min,
+      timezone: responseData.timezone,
       units,
-      weather,
+      weather: responseData.weather,
+      wind_deg: responseData.wind.deg,
+      wind_speed: responseData.wind.speed,
     };
 
     res.json(data);
