852e21c265e97cf90ddd42c4477ae053a4c487f2
Jesse Jackson <jsejcksn@users.noreply.github.com>
2020-02-23 18:44:27 -0600

    Add forecast endpoint (#13)

--- api/_utils/open-weather-api.ts
+++ api/_utils/open-weather-api.ts
@@ -1,14 +1,14 @@
 import fetch from 'node-fetch';
 
-export enum Units {
+export type OWMParams = {[key: string]: string};
+
+export enum OWMUnits {
   Imperial = 'imperial',
   Metric = 'metric',
   Standard = 'standard',
 }
 
-export type OWMParams = {[key: string]: string};
-
-export type WeatherCondition = {
+export type OWMWeatherCondition = {
   description: string;
   icon: string;
   id: number;
@@ -19,7 +19,7 @@ export type OWMCurrentWeatherData = {
   clouds: {
     all: number;
   };
-  cod: number;
+  cod: number | string;
   coord: {
     lat: number;
     lon: number;
@@ -49,11 +49,57 @@ export type OWMCurrentWeatherData = {
   };
   timezone: number;
   visibility: number;
-  weather: WeatherCondition[];
+  weather: OWMWeatherCondition[];
   wind: {
     deg: number;
     speed: number;
   };
+};
+
+export type OWMForecastItem = {
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  dt_txt: string;
+  main: {
+    feels_like: number;
+    grnd_level: number;
+    humidity: number;
+    pressure: number;
+    sea_level: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  rain?: {
+    '3h': number;
+  };
+  snow?: {
+    '3h': number;
+  };
+  weather: OWMWeatherCondition[];
+  wind: {
+    deg: number;
+    speed: number;
+  };
+};
+
+export type OWMForecastWeatherData = {
+  city: {
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    name: string;
+    sunrise: number;
+    sunset: number;
+    timezone: number;
+  };
+  cnt: number;
+  cod: number | string;
+  list: OWMForecastItem[];
 };
 
 export class OpenWeather {
@@ -88,7 +134,43 @@ export class OpenWeather {
       const json: OWMCurrentWeatherData = await response.json();
       const statusCodeOk = 200;
 
-      if (json.cod !== statusCodeOk) throw new Error('API response not OK');
+      if (
+        json.cod !== statusCodeOk
+        && json.cod !== '200'
+      ) throw new Error('API response not OK');
+
+      return json;
+    };
+
+  fetchForecast =
+    async (params: OWMParams = {}): Promise<OWMForecastWeatherData> => {
+      const url = new URL('https://api.openweathermap.org/data/2.5/forecast');
+      const searchParams = new URLSearchParams();
+      /* eslint-disable-next-line no-invalid-this */
+      searchParams.set('appid', this.apiKey);
+
+      for (const [param, value] of Object.entries(params)) {
+        if (value !== '') searchParams.set(param, value);
+      }
+
+      url.search = searchParams.toString();
+      const response = await fetch(url.toString());
+
+      if (!response.ok) {
+        throw Object.assign(new Error(), {
+          message: 'Fetch response not OK',
+          name: 'FetchError',
+          response,
+        });
+      }
+
+      const json: OWMForecastWeatherData = await response.json();
+      const statusCodeOk = 200;
+
+      if (
+        json.cod !== statusCodeOk
+        && json.cod !== '200'
+      ) throw new Error('API response not OK');
 
       return json;
     };
